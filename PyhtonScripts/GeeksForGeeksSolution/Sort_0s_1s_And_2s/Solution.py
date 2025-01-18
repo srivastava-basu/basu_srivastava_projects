1052c20eaf2ae1df71cff1b17e011c52a433d707
srivastava-basu <87208430+srivastava-basu@users.noreply.github.com>
2025-01-18 10:59:48 +0530

    Create Solution.py - Sort 0s, 1s and 2s

---
+++ PyhtonScripts/GeeksForGeeksSolution/Sort_0s_1s_And_2s/Solution.py
@@ -0,0 +1,29 @@
+### PROBLEM STATEMENT ###
+#Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        # code here
+        arr.sort()
+        return arr
+
+#{ 
+ # Driver Code Starts.
+def main():
+    t = int(input().strip())  # Read the number of test cases
+    ob = Solution()
+
+    while t > 0:
+        t -= 1
+        arr = list(map(int,
+                       input().strip().split())
+                   )  # Read the array as space-separated integers
+        ob.sort012(arr)  # Sort the array
+
+        print(' '.join(map(str, arr)))  # Print the sorted array
+        print("~")
+        
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
